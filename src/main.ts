8555b6729509e8b12e7ff5533a899246f9d8b3c9
gordonmenzies <heymish96@gmail.com>
2024-03-11 20:26:18 +0000

    calculating a negative now has a partial fix however it does not work when making multiple calculations with negative numbers, further information available in the readme

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ let firstNumberSet: number = 0;
 let secondNumberSet: number = 0;
 let storedOperator: string = "";
 let showingCalculation: boolean = false;
+let negative: boolean = true;
 
 /*
   OPERATOR FUNCTIONS
@@ -34,7 +35,6 @@ const plusMinus = (): void => {
   // Catch possible Null Values
   if (display.textContent === "0") {
     display.textContent = "-";
-    console.log(display.textContent);
   } else if (displayContent > 0) {
     displayContent = 0 - displayContent;
     display.textContent = String(displayContent);
@@ -44,12 +44,19 @@ const plusMinus = (): void => {
   } else {
     display.textContent = "0";
   }
+
+  console.log("firstNumberSet " + firstNumberSet);
+  console.log("secondNumberSet " + secondNumberSet);
+  console.log("stored operator " + storedOperator);
+  console.log("display " + display.textContent);
+  console.log("justchecking");
 };
 
 const clear = (): void => {
   display.textContent = "";
   firstNumberSet = 0;
   storedOperator = "";
+  negative = false;
   console.log("firstNumberSet " + firstNumberSet);
   console.log("secondNumberSet " + secondNumberSet);
   console.log("stored operator " + storedOperator);
@@ -62,23 +69,46 @@ const percentage = (): void => {
 };
 
 const decimal = (): void => {
-  display.textContent += ".";
+  if (display.textContent?.includes(".")) {
+    return;
+  } else {
+    display.textContent += ".";
+  }
 };
 
+/* reads the function of the button and determines the action to be performed 
+      set first number set
+      set second number set and calculate 
+   
+   stores the mathmatical function operator
+*/
 const acceptOperator = (event: Event): void => {
   if (!event.target) {
     throw new Error("button has no value");
   }
 
+  /* determines whether or not an equals operation is due to be performed 
+   or not 
+*/
   if (storedOperator === "") {
     firstNumberSet = Number(display.textContent);
     display.textContent = "";
+    storedOperator = event.target.textContent;
+
+    /* in the instance that a number that a number that 
+    would be given to the secondNumberSet is negative
+  */
+  } else if (storedOperator !== "" && event.target.textContent === "-") {
+    negative = true;
+
+    /* in the instance a number has already been entered and the use of the mathmatical operator
+    also performs a calculation
+  */
   } else {
     secondNumberSet = Number(display.textContent);
     equals(storedOperator);
+    storedOperator = event.target.textContent;
   }
-
-  storedOperator = event.target.textContent;
 
   console.log("firstNumberSet " + firstNumberSet);
   console.log("secondNumberSet " + secondNumberSet);
@@ -164,16 +194,37 @@ const equals = (passedOperator: string): void => {
 
 /* function that ajudicates what should be 
    displayed on screen when a button is pressed
- */
+*/
 const acceptNumber = (event: Event) => {
   if (!event.target) {
     throw new Error("button has no value");
   }
+
+  /* if display is currently showing a calculated value rather than an inputted value or no value
+   */
   if (showingCalculation === true) {
     display.textContent = String(event.target.textContent);
     showingCalculation = false;
+
+    /* if display is showing zero 
+      after calculation 
+      initial input (times zero or decimal)
+      or after use of plus minus when display is empty 
+  */
   } else if (display.textContent === "0") {
     display.textContent = String(event.target.textContent);
+
+    /* if the display is showing a negative number 
+              after calculation 
+              initial input
+  */
+  } else if (negative === true) {
+    display.textContent = String(`-${event.target.textContent}`);
+    negative = false;
+    /* if the number is greater than 9 figures, 
+              after calculation 
+              initial input
+  */
   } else if (display.textContent.length > 8) {
     return;
   } else {
@@ -184,6 +235,7 @@ const acceptNumber = (event: Event) => {
   console.log("secondNumberSet " + secondNumberSet);
   console.log("stored operator " + storedOperator);
   console.log("display " + display.textContent);
+  console.log("justchecking");
 };
 
 /*
